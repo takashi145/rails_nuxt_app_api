ff0f459c267f57012e86515c1715c2e767c999c9
takashi145 <t.shuhei0203@gmail.com>
2022-11-20 11:59:18 +0900

    add users_controller, index method

--- app/controllers/api/v1/hello_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::HelloController < ApplicationController
-
-  def index
-    render json: "Hello"
-  end
-end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::UsersController < ApplicationController
+
+  def index
+    # ユーザー全取得
+    users = User.all
+    render json: users.as_json(only: [:id, :name, :email, :created_at])
+  end
+end
