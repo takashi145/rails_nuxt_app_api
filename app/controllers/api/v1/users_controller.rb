01f790c02322c64896d7aaee8becf6479e5a40b5
takashi145 <t.shuhei0203@gmail.com>
2022-12-10 01:49:41 +0900

    authenticate service module

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user
 
   def index
-    # ユーザー全取得
-    users = User.all
-    render json: users.as_json(only: [:id, :name, :email, :created_at])
+    # users = User.all
+    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  include UserAuthenticateService
+  
 end
